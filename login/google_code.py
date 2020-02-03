2d3ceadca6c338700b8683874b6f1ce515ace7c7
peng.shaocheng <peng.shaocheng@immomo.com>
2020-02-03 18:17:43 +0800

    Feature: add a python script to parse google auth code

---
+++ login/google_code.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import pyotp
+
+"""
+This python script is used to parse google auth code.
+"""
+
+
+def get_google_code(token):
+    """
+    Parse google auth code
+    :param token: token
+    :return: code with six numbers
+    """
+    totp = pyotp.TOTP(token)
+    return totp.now()
+
+
+# replace to your own token
+print get_google_code("abc")
