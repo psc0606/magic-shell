874e10837293667048320c61bae5a4d31daac7e2
pengshaocheng <psc0606@163.com>
2020-03-15 12:17:12 +0800

    Feature: storm-logs-cleaner.py to python3

--- modules/cleaner/storm-logs-cleaner.py
+++ modules/cleaner/storm-logs-cleaner.py
@@ -1,4 +1,4 @@
-#!/bin/env python2
+#!/bin/env python3
 # coding=utf-8
 import os
 
@@ -23,13 +23,13 @@ def get_storm_logs_dir(path):
             if split_name_and_time(old)[1] < job_start_time:
                 storm_jobs[storm_job_name] = d
 
-    print '保留:'
-    print storm_jobs.values()
+    print('保留:')
+    print(storm_jobs.values())
     to_deleted_dirs = [i for i in dirs if i not in storm_jobs.values()]
     for d in to_deleted_dirs:
         # 仅删目录
         if os.path.isdir(d):
-            print '删除目录:%s' % d
+            print('删除目录:{}'.format(d))
             cmd = 'rm -fr ' + d
             os.system(cmd)
             # os.removedirs(d)
@@ -43,11 +43,11 @@ def split_name_and_time(path):
 
 def do_clean():
     LOG_PATH = '/tmp/soft/apache-storm-1.1.0/logs/workers-artifacts'
-    print os.getcwd()
+    print(os.getcwd())
     if os.getcwd() == LOG_PATH:
         get_storm_logs_dir('')
         return
-    print '不在%s' % LOG_PATH
+    print('不在%s' % LOG_PATH)
 
 
 if __name__ == '__main__':
