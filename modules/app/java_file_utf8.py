7410b6f1d1b25764a8fc395622446967de4b181b
pengshaocheng <psc0606@163.com>
2020-03-15 19:03:49 +0800

    Feature: add java_file_utf8.py to change java file encoding to utf-8

--- modules/app/java_file_utf8.py
+++ modules/app/java_file_utf8.py
@@ -8,12 +8,13 @@ from modules.FileEncodingTranslator import FileEncodingTranslator
 
 
 def callback(path, *args, **kwargs):
+    print("Now translate file {}".format(path))
     translator = FileEncodingTranslator(path)
-    translator.translate_encoding("UTF-8", False)
+    translator.translate_encoding("UTF-8", True)
 
 
 def file2utf8(root_path):
-    traversal = DirectoryTraversal(root_path)
+    traversal = DirectoryTraversal(root_path, ['.idea', '.git'], ['A.java'])
     traversal.traverse(callback=callback)
     print(root_path)
 
